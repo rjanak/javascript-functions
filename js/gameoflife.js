3fc51890bd5310768fff36f3a7b80a6b000cf03c
Rahul Janak <rhljnk@gmail.com>
2021-12-14 15:01:24 +0300

    third

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -8,7 +8,7 @@ function same([x, y], [j, k]) {
 
 // The game state to search for `cell` is passed as the `this` value of the function.
 function contains(cell) {
-
+   return this.some((c) => same(cell, c));
 }
 
 const printCell = (cell, state) => {};
